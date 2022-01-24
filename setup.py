182537dbead8cbf05affd4db121a68fcb3c10cc3
ctan <cyztan@gmail.com>
2022-01-24 15:41:34 -0800

    Initial setup for package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='tensordata',
+      version='0.0.1',
+      description='A common repository for tensor structured datasets.',
+      url='https://github.com/meyer-lab/tensordata',
+      license='MIT',
+      packages=find_packages(exclude=['doc']),
+      install_requires=['numpy', 'tensorly'])
