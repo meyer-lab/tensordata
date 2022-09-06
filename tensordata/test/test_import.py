d95cdecb5d22461a48338979995e2e5e1d2f8c4f
jstickelmaier <jamie.stickelmaier@gmail.com>
2022-09-06 08:19:02 -0700

    Remove bunch from tests

--- tensordata/test/test_import.py
+++ tensordata/test/test_import.py
@@ -1,57 +1,46 @@
 def test_atyeo():
     from ..atyeo import data
-    d = data()
-    shp = d.tensor.shape
-    for ii in range(3):
-        assert shp[ii] == len(d.axes[ii])
-
-    dx = data(xarray = True)
+    
+    dx = data()
     assert len(dx.sel(Antigen = 'S').shape) == 2
 
 
 def test_alter():
     from ..alter import data
-    d = data()
-    shp = d.tensor.shape
-    for ii in range(3):
-        assert shp[ii] == len(d.axes[ii])
-    assert d.tensor.shape[0] == d.matrix.shape[0]
-    assert d.matrix.shape[1] ==  len(d.axes[3])
-
-    ds = data(xarray=True)
+    
+    ds = data()
     assert "Fc" in ds
     assert "gp120" in ds
 
+    dx = ds.to_array()
+    assert len(dx.sel(variable='Fc').shape) == 4
+    assert len(dx.sel(variable='gp120').shape) == 4
+
 def test_zohar():
-    from ..zohar import data, data3D
-    d = data()
-    shp = d.tensor.shape
-    for ii in range(4):
-        assert shp[ii] == len(d.axes[ii])
+    from ..zohar import data
 
-    dx = data(xarray=True)
-    assert len(dx.sel(Antigen='S1').shape) == 3
-
-    d3 = data3D()
-    shp3 = d3.tensor.shape
-    for ii in range(3):
-        assert shp3[ii] == len(d3.axes[ii])
-
-    dx3 = data3D(xarray=True)
-    assert len(dx3.sel(Antigen='RBD').shape) == 2
+    dx = data()
+    assert len(dx.sel(Antigen='S1').shape) == 2
+    assert len(dx.sel(Receptor='IgG3').shape) == 2
 
 def test_kaplonek():
-    from ..kaplonek import SpaceX, MGH, MGHds
-    s = SpaceX()
-    shp = s.tensor.shape
-    for ii in range(3):
-        assert shp[ii] == len(s.axes[ii])
-    sx = SpaceX(xarray=True)
-    assert len(sx.sel(Receptor='IgA').shape) == 2
+    from ..kaplonek import SpaceX4D, MGH4D
+    sx = SpaceX4D()
+    assert len(sx.sel(Receptor='IgA').shape) == 3
 
-    m = MGH()
-    mhp = m.tensor.shape
-    for ii in range(3):
-        assert mhp[ii] == len(m.axes[ii])
-    ds = MGHds()
-    assert len(ds["Fc"].sel(Antigen='CMV').shape) == 3
+    mx = MGH4D()
+    assert len(mx.sel(Antigen='CMV').shape) == 3
+
+def test_kaplonekVaccine():
+    from ..kaplonekVaccine import data
+
+    dx = data()
+    assert len(dx.sel(Antigen='RBD').shape) == 2
+    assert len(dx.sel(Receptor='IgG3').shape) == 2
+
+def test_chung():
+    from ..chung import data
+
+    dx = data()
+    assert len(dx.sel(Antigen='SARS2 Trimer').shape) == 2
+    assert len(dx.sel(Receptor='IgM').shape) == 2
