4e15dc57b60746d663e455f0ad633a9d5181734b
jstickelmaier <jamie.stickelmaier@gmail.com>
2022-10-19 14:15:21 -0700

    Fix shaping

--- tensordata/functional.py
+++ tensordata/functional.py
@@ -8,10 +8,19 @@ def plsr():
     model = CP_PLSR(4)
 
     serology = MGH4D()["Serology"].stack(Sample = ("Subject", "Time"))
-    X = np.transpose(serology.values)
+    finite_ser_ind = np.all(np.isfinite(serology.values), axis=(0,1))
+    
 
     functional = MGH4D()["Functional"].stack(Sample = ("Subject", "Time"))
-    Y = np.transpose(functional.values)
+    finite_func_ind = np.all(np.isfinite(functional.values), axis=0)
+    
+    finite_ind = np.logical_and(finite_ser_ind, finite_func_ind)
+    
+
+    finite_func = functional.isel(Sample=finite_ind)
+    finite_ser = serology.isel(Sample=finite_ind)
+    X = np.transpose(finite_ser.values)
+    Y = np.transpose(finite_func.values)
 
     model.fit(X, Y)
 
