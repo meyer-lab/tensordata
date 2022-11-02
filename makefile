a8ee4477b0cfd4bedb8aac68c8750b0f9e23b82b
jstickelmaier <jamie.stickelmaier@gmail.com>
2022-11-02 12:18:16 -0700

    Setup figures

--- makefile
+++ makefile
@@ -10,3 +10,7 @@ coverage.xml:
 
 clean:
 	rm -rf coverage.xml
+
+output/figure%.svg: tensordata/figures/figure%.py
+	mkdir -p output
+	XLA_PYTHON_CLIENT_MEM_FRACTION=0.1 poetry run fbuild $*
--- tensordata/functional.py
+++ tensordata/functional.py
@@ -17,24 +17,18 @@ def calcR2X(tOrig, tFac):
 
 
 def plsr():
-
-
     serology = MGH4D()["Serology"].stack(Sample = ("Subject", "Time"))
     finite_ser_ind = np.all(np.isfinite(serology.values), axis=(0,1))
-    
 
     functional = MGH4D()["Functional"].stack(Sample = ("Subject", "Time"))
     finite_func_ind = np.all(np.isfinite(functional.values), axis=0)
     
     finite_ind = np.logical_and(finite_ser_ind, finite_func_ind)
     
-
     finite_func = functional.isel(Sample=finite_ind)
     finite_ser = serology.isel(Sample=finite_ind)
     X = np.transpose(finite_ser.values)
     Y = np.transpose(finite_func.values)
-
-
 
     for rr in range(1, 6):
         model = CP_PLSR(rr)
