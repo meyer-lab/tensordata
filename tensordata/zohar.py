2498ef38f6081959da2257959eefae4433879707
Cyrillus Tan <cyztan@gmail.com>
2022-11-04 15:47:56 -0700

    Update zohar.py

--- tensordata/zohar.py
+++ tensordata/zohar.py
@@ -6,6 +6,25 @@ import pandas as pd
 
 path_here = dirname(dirname(__file__))
 
+def pbsSubtractOriginal():
+    """ Paper Background subtract, will keep all rows for any confusing result. """
+    Cov = pd.read_csv(join(path_here, "tensordata/zohar2020/ZoharCovData.csv"), index_col=0)
+    # 23 (0-> 23) is the start of IgG1_S
+    Demographics = Cov.iloc[:, 0:23]
+    Serology = Cov.iloc[:, 23::]
+    Serology -= Serology.loc["PBS"].values.squeeze()
+    df = pd.concat([Demographics, Serology], axis=1)
+    df = df.loc[np.isfinite(df["patient_ID"]), :]
+    df["week"] = np.array(df["days"] // 7 + 1.0, dtype=int)
+    df["patient_ID"] = df["patient_ID"].astype('int32')
+    df["group"] = pd.Categorical(df["group"], ["Negative", "Mild", "Moderate", "Severe", "Deceased"])
+    df = df.sort_values(by=["group", "days", "patient_ID"])
+    df = df.reset_index()
+    # Get rid of any data that doesn't have a time component (i.e. "nan" for day)
+    df = df.dropna(subset=["days"]).reset_index(drop=True)
+    return df
+
+
 def data():
 
     df = pd.read_csv(join(path_here, "tensordata/zohar2020/ZoharCovData.csv"))
